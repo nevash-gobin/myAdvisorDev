27cc838addcc652b0acb03409d9370562d6d5e80
Nevash Gobin <nevash19@gmail.com>
2021-02-23 20:04:34 -0400

    Fixed the routes

--- myadvisor/src/App.js
+++ myadvisor/src/App.js
@@ -4,15 +4,22 @@ import Main from './components/Main';
 import './App.css';
 import { Component } from "react";
 import SignInSide from "./components/login";
+import StudentProfile from "./components/StudentProfile";
+import Career from "./components/Career";
+import CourseDetails from "./components/CourseDetails";
+import CourseList from "./components/CourseList";
 
 class App extends Component {
   render() {
     return (
         <Router>
           <Switch>
-            <Route path="/" component={StaffMain} />
+            <Route path="/staff" component={StaffMain} />
             <Route path="/login" component={SignInSide}/>
-          
+            <Route path="/home" component={StudentProfile}/>
+            <Route path="/courses" component={CourseList} />
+            <Route path="/coursedetails" component={CourseDetails} />
+            <Route path="/career" component={Career} />
           </Switch>
         </Router>
     );
