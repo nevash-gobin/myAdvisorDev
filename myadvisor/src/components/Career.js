32017285d586596d2a2d760e35894eb824e6b397
MatthewChristian <matthewjlchristian@yahoo.com>
2021-04-09 23:07:57 -0400

    Career form reads values

--- myadvisor/src/components/Career.js
+++ myadvisor/src/components/Career.js
@@ -1,14 +1,86 @@
 import React, { Component } from "react";
+import { useEffect, useState } from "react";
 import CareerCheck from './CareerCheck';
 import "../assets/css/Career.css";
 import { Link } from 'react-router-dom'
 import PullCareers from "./PullCareers";
+import axios from "axios";
 
 const Career = (props) => {
+
+  const [careerList, setCareerList] = useState([]);
 
   var careers = PullCareers();
   props.setDisplay(false);
   props.setProg(40);
+
+  function onChange(event) { 
+    console.log(event.currentTarget.value, event.currentTarget.checked);
+    var careerArray = careerList;
+    if (event.currentTarget.checked === true) {
+      careerArray.push(event.currentTarget.value);
+    }
+    else {
+      var index = careerArray.indexOf(event.currentTarget.value);
+      if (index > -1) {
+        careerArray.splice(index, 1);
+      }
+    }
+    setCareerList(careerArray);
+  }
+
+  function onClick() {
+    console.log(careerList);
+    determineCourses(careerList);
+  }
+
+  async function getCareerCourses(id) {
+    try {
+      const {data:response} = await axios.get(`http://localhost:5000/careers/courses/${id}`) //use data destructuring to get data from the promise object
+      return response
+    }
+    catch (error) {
+      console.log(error);
+    }
+}
+
+async function determineCourses(careerArray){
+
+
+  for (var i=0; i<careerArray.length; i++) {
+    var courses = await getCareerCourses(careerArray[i]);
+    console.log(courses);
+  }
+
+ /* var recCourses = [];
+  var today = new Date();
+  var currentSem;
+  
+  if (today.getMonth() < 4) {
+      currentSem = "2";
+  }
+  else if (today.getMonth() < 6) {
+      currentSem = "3";
+  }
+  else {
+      currentSem = "1";
+  }
+
+  var courses = await getProgrammeCourses(programmeId);
+
+  for (var i=0; i<courses.length; i++) {
+      if (courses[i].type === "Core" && courses[i].semester === currentSem && courses[i].level === "I") {
+          recCourses.push(courses[i].courseCode);
+      }
+  }
+
+  props.setRecommended(recCourses);
+
+  history.push({
+      pathname: '/courses'
+  }) */
+  
+}
 
 
   return (
@@ -29,7 +101,7 @@ const Career = (props) => {
                                 </tr>
                             </thead>
                             <tbody>
-                              <CareerCheck careers={careers}></CareerCheck>
+                              <CareerCheck careers={careers} onChange={onChange}></CareerCheck>
                             </tbody>
                           </table>
                         </div>
@@ -41,9 +113,9 @@ const Career = (props) => {
                         </Link>
                       </div>          
                       <div className="col-sm-2 offset-sm-8">
-                      <Link to="/courses">
-                        <button type="submit" class="btn btn-custom career-next-button blue-button">Next</button>
-                        </Link>
+                      
+                        <button type="button" class="btn btn-custom career-next-button blue-button" onClick={onClick}>Next</button>
+                        
                       </div>
                     </div>
                   </form>
--- myadvisor/src/components/CareerCheck.js
+++ myadvisor/src/components/CareerCheck.js
@@ -7,7 +7,7 @@ class CareerCheck extends Component {
                 Array.from({ length: this.props.careers.length }, (_, k) => (
                     <tr class="d-flex career-row">
                         <td class="col-3 name-cell form-check">
-                            <input type="checkbox" class="form-check-input career-check" id={`careerCheck${k}`}></input>
+                            <input type="checkbox" class="form-check-input career-check" id={`careerCheck${k}`} value={this.props.careers[k].id} onChange={this.props.onChange}></input>
                             <label className="form-check-label career-label" for={`careerCheck${k}`}>{this.props.careers[k].name}</label>
                         </td>
                         <td class="col-9 desc-cell">{this.props.careers[k].description}</td>
