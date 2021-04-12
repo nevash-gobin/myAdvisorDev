a3042817363f4ef968d9e5dd07f13660ae34add0
Nevash Gobin <nevash19@gmail.com>
2021-04-12 17:37:29 -0400

    chat bot added

--- myadvisor/src/components/Bot Framework/webChat.js
+++ myadvisor/src/components/Bot Framework/webChat.js
@@ -3,7 +3,8 @@ import React, { useMemo } from 'react';
 import ReactWebChat, { createDirectLine } from 'botframework-webchat';
 
 export default () => {
-  const directLine = useMemo(() => createDirectLine({ token: 'XFSQFxuEYr0.wmewXCmoOG1b5yq7vvu_LBc2EycCMqnrjdb7yxGytvg' }), []);
+  const directLine = useMemo(() => createDirectLine({ token: '62vtFFB3kyk.SDMvOliF963GN1Hm7MG-7XJ9qLGNKB-Unv1L_1-9dnc' }), []);
 
-  return <ReactWebChat directLine={directLine} userID="myAdvisor" />;
+  return <ReactWebChat directLine={directLine} userID="myAdvisorBot" />;
 };
+
