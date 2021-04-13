8c41a5c8cb99107d7b4f6610297aa5f3fb321ff9
Nevash Gobin <nevash19@gmail.com>
2021-04-13 17:47:05 -0400

    Bot style changes

--- myadvisor/src/components/Bot Framework/webChat.js
+++ myadvisor/src/components/Bot Framework/webChat.js
@@ -1,10 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
 
 import React, { useMemo } from 'react';
 import ReactWebChat, { createDirectLine } from 'botframework-webchat';
+import './webChatStyle.css';
 
 export default () => {
-  const directLine = useMemo(() => createDirectLine({ token: '62vtFFB3kyk.SDMvOliF963GN1Hm7MG-7XJ9qLGNKB-Unv1L_1-9dnc' }), []);
 
-  return <ReactWebChat directLine={directLine} userID="myAdvisorBot" />;
+  //Azure Direct Line Token:
+  const token = "62vtFFB3kyk.SDMvOliF963GN1Hm7MG-7XJ9qLGNKB-Unv1L_1-9dnc";
+
+  const directLine = useMemo(() => createDirectLine({ token}),[]);
+  
+  
+  return <ReactWebChat className="style" directLine={directLine} userID="myAdvisorBot" />;
+  
 };
 
