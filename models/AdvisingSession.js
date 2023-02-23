8fdcf90d85281e9e5d14b95d0691ad107f820ec2
datalegacy <datalegacy@sundaebytes.com>
2023-02-23 22:31:38 +0000

    updated files

--- models/AdvisingSession.js
+++ models/AdvisingSession.js
@@ -1,4 +1,4 @@
-/*
+
 const { Sequelize } = require("sequelize");
 const db = require("../db");
 
@@ -14,4 +14,4 @@ const AdvisingSession = db.define("advisingsession", {
 });
 
 module.exports = AdvisingSession;
- */
+ 
--- models/AdvisingWindow.js
+++ models/AdvisingWindow.js
@@ -1,4 +1,3 @@
-/*
 
 const { Sequelize } = require("sequelize");
 const db = require("../db");
@@ -19,4 +18,3 @@ const AdvisingWindow = db.define("advisingwindow", {
 });
 
 module.exports = AdvisingWindow;
- */
