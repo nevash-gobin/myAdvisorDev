56b491a079a593d59824a6195574a28e1e6b503b
jonathonwingson <jonathonwingson@gmail.com>
2023-06-22 12:30:33 +0000

    localhost url remove + config file added

--- utilities/parser.js
+++ utilities/parser.js
@@ -46,18 +46,11 @@ function decode(token){
 
 async function getCourses() {
     try {
-      const {data:response} = await axios.get('http://localhost:5000/courses/all') //use data destructuring to get data from the promise object
+      const {data:response} = await axios.get(`${config.host}/courses/all`) //use data destructuring to get data from the promise object
       return response
     }
     catch (error) {
       console.log(error);
-      try { // If app is in production
-        const {data:response} = await axios.get('https://myadvisorapp.herokuapp.com/courses/all') //use data destructuring to get data from the promise object
-        return response
-      }
-      catch (error2) {
-        console.log(error2);
-      }
     }
   }
 
