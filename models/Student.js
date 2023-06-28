466ef55adefaf293ccecd7ac41b7f1eaa624feb5
jonathonwingson <jonathonwingson@gmail.com>
2023-06-28 20:02:13 +0000

    add new attributes to student model

--- models/Student.js
+++ models/Student.js
@@ -1,18 +1,32 @@
 const { Sequelize } = require("sequelize");
 const db = require("../db");
 
-// NOTE: Student model requires more fields for grades, year etc.
-
-    const Student = db.define("student", {
-        username: {
-            allowNull: false,
-            type: Sequelize.STRING,
+const Student = db.define("student", {
+    studentID: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+    },
+    firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+    },
+    lasttName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+    },
+    email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+        validate: {
+            isEmail: true,
         },
-        password: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-    }, {timestamps: true});
-    
+    },
+    password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+    },
+}, { timestamps: true });
 
 module.exports = Student;
