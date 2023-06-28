df5ddf78504404a8b89d36c0ab6f1c7eeb5d6e8e
jonathonwingson <jonathonwingson@gmail.com>
2023-06-28 20:17:21 +0000

    AdvisingSession model changed

--- models/AdvisingSession.js
+++ models/AdvisingSession.js
@@ -1,16 +1,10 @@
-
-const { Sequelize } = require("sequelize");
+//const { Sequelize } = require("sequelize");
 const db = require("../db");
 
 const AdvisingSession = db.define("advisingsession", {
-    studentID: {
-        allowNull: false,
-        type: Sequelize.STRING,
-    },
-    sessionDate: {
-        allowNull: false,
-        type: Sequelize.DATEONLY
-    }
+
+}, {
+    timestamps: true
 });
 
 module.exports = AdvisingSession;
