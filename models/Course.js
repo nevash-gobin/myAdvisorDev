52b217c17f6e6ff529895293746c441e0b509e5e
Hex-006 <mjah0218@gmail.com>
2024-02-14 20:07:01 -0400

    Course Model updated

--- models/Course.js
+++ models/Course.js
@@ -13,7 +13,7 @@ const Course = db.define("course", {
     },
     credits: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
     },
     level: {
         allowNull: false,
@@ -21,84 +21,85 @@ const Course = db.define("course", {
     },
     semester: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.ENUM,
+        values:['I','II','III']
     },
-    department: {
-        allowNull: false,
-        type: Sequelize.STRING,
-    },
+    // department: {
+    //     allowNull: false,
+    //     type: Sequelize.STRING,
+    // },
     description: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,//Datatype = TEXT to allow for descriptions longer than 255
     },
-    coursework: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    finalExam: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    groupProject: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    individualWork: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    practicalCoursework: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    courseworkExam: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    projectPres: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    project: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    presentation: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    assignment: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    labAssessment: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    midSemesterMcq: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    projectReport: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    projectReportPres: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    projectAndPres: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    performanceReports: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    },
-    projectSoftwareApp: {
-        allowNull: true,
-        type: Sequelize.STRING,
-    }
+    // coursework: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // finalExam: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // groupProject: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // individualWork: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // practicalCoursework: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // courseworkExam: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // projectPres: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // project: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // presentation: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // assignment: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // labAssessment: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // midSemesterMcq: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // projectReport: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // projectReportPres: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // projectAndPres: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // performanceReports: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // },
+    // projectSoftwareApp: {
+    //     allowNull: true,
+    //     type: Sequelize.STRING,
+    // }
     
 });
 
