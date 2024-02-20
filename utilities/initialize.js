ea002baa61677a2c2a5fc35323171ff3677f18f2
Quinn <quinn.mohamed12@gmail.com>
2024-02-20 12:37:54 -0400

    edited init script

--- utilities/initialize.js
+++ utilities/initialize.js
@@ -1,27 +1,9 @@
 //import all models and insert data into the database
 
 // models
-const Admin = require("../models/Admin");
-const AdvisedCourse = require("../models/SelectedCourse");
-const AdvisingSesssion = require("../models/AdvisingSession")
-const Antirequisite = require("../models/Antirequisite");
-const AwardedDegree = require("../models/AwardedDegree");
-const Course = require("../models/Course");
-const ElectiveRequirement = require("../models/ElectiveRequirement");
-const PotentialGraduate = require("../models/PotentialGraduate");
-const Prerequisite = require("../models/Prerequisite");
-const Programme = require("../models/Programme");
-const ProgrammeCourse = require("../models/ProgrammeCourse");
-const Semester = require("../models/Semester");
-const Student = require("../models/Student");
-const StudentCourse = require("../models/StudentCourse");
-const Transcript = require("../models/Transcript");
-const Type = require("../models/Type");
-const Group = require("../models/Group");
-const CourseGroup = require("../models/CourseGroup");
-const SemesterCourse = require("../models/SemesterCourse");
-
-const db = { Student, Programme, Semester, Course, StudentCourse, Transcript, AwardedDegree, Group, SemesterCourse };
+const db = require('../models');
+const { Student, Programme, Semester, Course, StudentCourse, Transcript, AwardedDegree, Group, SemesterCourse } = require('../models');
 
 const initialize = async () => {
+
 };
